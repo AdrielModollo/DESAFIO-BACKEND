b86191df59b8cbb1fac085476e0870307390bae6
AdrielModollo <adrielmodollo@outlook.com>
2022-09-19 23:51:15 -0300

    feat: correction name birthdate

--- src/database/migrations/20220920023206-create-users.js
+++ src/database/migrations/20220920023206-create-users.js
@@ -17,7 +17,7 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      birthDate: {
+      birthday: {
         type: Sequelize.DATE,
         allowNull: false,
       },
