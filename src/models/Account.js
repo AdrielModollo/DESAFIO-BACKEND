ea1b285806db1445124bf0a44c329f9c13fa5c3e
Adriel Modollo <adrielmodollo@outlook.com>
2022-09-20 22:36:44 -0300

    feat: rename fields model

--- src/models/Account.js
+++ src/models/Account.js
@@ -3,10 +3,10 @@ const { Model, DataTypes } = require('sequelize');
 class Account extends Model {
     static init(sequelize){
         super.init({
-            saldo: DataTypes.DOUBLE,
-            debito: DataTypes.DOUBLE,
-            credito: DataTypes.DOUBLE,  
-            estorno: DataTypes.DOUBLE,
+            balance: DataTypes.DOUBLE,
+            debit: DataTypes.DOUBLE,
+            credit: DataTypes.DOUBLE,  
+            estorn: DataTypes.DOUBLE,
             total: DataTypes.DOUBLE,          
         }, {
             sequelize
