49e675f6ad8379a11e4f83a6ce8a7ca1f1b67bbf
Adriel Modollo <adrielmodollo@outlook.com>
2022-09-21 00:48:26 -0300

    feat: Associate models

--- src/database/index.js
+++ src/database/index.js
@@ -2,9 +2,13 @@ const Sequelize = require('sequelize');
 const dbConfig = require('../config/database')
 
 const User = require('../models/User')
+const Account = require('../models/Account')
 
 const connection = new Sequelize(dbConfig);
 
-User.init(connection)
+User.init(connection);
+Account.init(connection);
+
+Account.associate(connection.models);
 
 module.exports = connection;
