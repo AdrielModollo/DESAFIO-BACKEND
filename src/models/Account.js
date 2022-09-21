1573554dfc7d9c52737e16b82852003d96e084f0
Adriel Modollo <adrielmodollo@outlook.com>
2022-09-21 17:15:08 -0300

    feat: account and user

--- src/models/Account.js
+++ src/models/Account.js
@@ -3,10 +3,10 @@ const { Model, DataTypes } = require('sequelize');
 class Account extends Model {
     static init(sequelize){
         super.init({
-            balance: DataTypes.DOUBLE,
             debit: DataTypes.DOUBLE,
             credit: DataTypes.DOUBLE,  
             estorn: DataTypes.DOUBLE,
+            total: DataTypes.DOUBLE,
         }, {
             sequelize
         })
