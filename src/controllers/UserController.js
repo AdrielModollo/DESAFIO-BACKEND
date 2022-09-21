4bec445e7adf9046b2c087e36b75f2938179d95a
Adriel Modollo <adrielmodollo@outlook.com>
2022-09-20 22:21:35 -0300

    feat: remove store user controller

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -27,14 +27,6 @@ module.exports = {
         return res.json(user)
     },
 
-    async store(req, res) {
-        const { name, email, birthday } = req.body;
-
-        const user = await User.create({ name, email, birthday });
-
-        return res.json(user)
-    },
-
     async deleteOne(req, res) {
         const { user_id } = req.params;
 
