56ea8fd8b5c245f98494c113338b6c121d2094fc
Adriel Modollo <adrielmodollo@outlook.com>
2022-09-21 23:29:54 -0300

    feat: age validation

--- src/presentation/controllers/schemas/create-schema.js
+++ src/presentation/controllers/schemas/create-schema.js
@@ -1,10 +1,15 @@
 const yup = require('yup');
+const { sub } = require("date-fns/fp")
+const { parseISO } = require("date-fns")
 
 const userCreate = yup.object({
   body: yup.object({
     name: yup.string().required(),
     email: yup.string().required(),
-    birthday: yup.date().required(),
+    birthday: yup.date()
+      .max(sub({ years: 18 }, new Date()), "User must be at least 18 years old")
+      .transform((_value, originalValue) => parseISO(originalValue))
+      .typeError('Date must be YYYY-MM-DD format'),
     balance: yup.number().required(),
   }),
 });
