14a93d99441687fbca50a37e21bd61bf584149ab
Adriel Modollo <adrielmodollo@outlook.com>
2022-09-21 22:04:56 -0300

    feat: global errors middlware

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const routes = require('./routes')
+const { globalErrorsMiddleware } = require('./main/middlewares/global-middlwares')
 
 require('./database')
 
 const app = express();
 
-app.use(express.json())
-app.use(routes)
+app
+    .use(express.json())
+    .use(routes)
+    .use(globalErrorsMiddleware);
 
 app.listen(3333);
