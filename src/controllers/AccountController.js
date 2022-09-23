1919da9fbe7a5fe9ad95bdf4f650be89e23d81dd
Adriel Modollo <adrielmodollo@outlook.com>
2022-09-23 16:48:39 -0300

    feat: new values

--- src/controllers/AccountController.js
+++ src/controllers/AccountController.js
@@ -1,18 +1,21 @@
 const Account = require('../models/Account');
 const User = require('../models/User');
+const { verifyNewValue } = require('./services/verifyNewValue')
 
 module.exports = {
     async create(req, res) {
         const { user_id } = req.params;
-        const { debit, credit, estorn, total } = req.body;
+        const { debit, credit, estorn } = req.body;
 
         const user = await User.findByPk(user_id);
 
-        if(!user) {
-            return res.status(400).json({ error: "User not found "})
+        if (!user) {
+            return res.status(400).json({ error: "User not found " })
         }
 
-        const account = await Account.create({ debit, credit, estorn, total, user_id})
+        const total = verifyNewValue(debit, credit, estorn, user.balance)
+
+        const account = await Account.create({ debit, credit, estorn, total, user_id })
 
         return res.json(account)
     },
